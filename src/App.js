78b59ccd310b21135e44a077a33f49b730a64e9d
Lucas Nahuel Diaz <ldiaz@lnahueldiaz-ltm.internal.salesforce.com>
2019-12-03 00:02:07 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tabletop from 'tabletop';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Table,
+  Row,
+  Col
+} from "reactstrap";
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    Tabletop.init({
+      key: '1hWHPMCms9pz6nkO1wSJ0rdRbV6Xq9aWaEj_8yIQsYAI',
+      callback: googleData => {
+        console.log('google sheet data --->', googleData)
+        this.setState({
+          data: googleData
+        })
+      },
+      simpleSheet: true
+    })
+  }
+
+
+render() {
+    const { data } = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Sanfra Demo</h1>
+        </header>
+        <div className="content">
+          <Row>
+            <Col md="12">
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h4">Tabla de posicines</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Table className="tablesorter" responsive>
+                    <thead className="text-primary">
+                      <tr>
+                        <th>Posicion</th>
+                        <th>Equipo</th>
+                        <th>PJ</th>
+                        <th>Puntos</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    { data.map(obj => (
+                      <tr>
+                        <td>{obj.POSICION}</td>
+                        <td>{obj.EQUIPO}</td>
+                        <td>{obj.PJ}</td>
+                        <td>{obj.Puntos}</td>
+                      </tr>
+                     ))} 
+                    </tbody>
+                  </Table>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
